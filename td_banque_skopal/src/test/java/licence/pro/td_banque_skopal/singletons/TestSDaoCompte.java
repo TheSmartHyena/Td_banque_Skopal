002801d9ab237cf2712487c9577d25232ef3c1b7
skopal philippe <skopal.philippe@gmail.com>
2020-05-07 12:48:24 +0200

    1.2
    Ajout du singleton SDaoCompte et test correct

--- td_banque_skopal/src/test/java/licence/pro/td_banque_skopal/singletons/TestSDaoCompte.java
+++ td_banque_skopal/src/test/java/licence/pro/td_banque_skopal/singletons/TestSDaoCompte.java
@@ -1,5 +1,35 @@
 package licence.pro.td_banque_skopal.singletons;
 
-public class TestSDaoCompte {
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import licence.pro.td_banque_skopal.dao.DaoCompte;
+
+public class TestSDaoCompte {
+	
+	private DaoCompte instance;
+	
+	@Before
+	public void setUp() {
+		instance = SDaoCompte.getInstance();
+	}
+	
+	@Test
+	public void TestTestSDaoCompte_notNull() {
+		assertNotNull(instance);
+	}
+	
+	@Test
+	public void TestTestSDaoCompte_isInstanceDaoClient() {
+		assertTrue(instance instanceof DaoCompte);
+	}
+	
+	@Test
+	public void TestTestSDaoCompte_shouldBeSame() {
+		DaoCompte result = SDaoCompte.getInstance();
+		assertTrue(result == instance);
+	}
 }
