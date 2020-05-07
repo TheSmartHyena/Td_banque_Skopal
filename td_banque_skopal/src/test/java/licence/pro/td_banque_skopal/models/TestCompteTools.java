17cb531ac08bc583a77d1584d65b24854bce8220
skopal philippe <skopal.philippe@gmail.com>
2020-05-07 13:18:23 +0200

    Ajout mockito mais erreur acces ?

--- td_banque_skopal/src/test/java/licence/pro/td_banque_skopal/models/TestCompteTools.java
+++ td_banque_skopal/src/test/java/licence/pro/td_banque_skopal/models/TestCompteTools.java
@@ -1,12 +1,17 @@
 package licence.pro.td_banque_skopal.models;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import licence.pro.td_banque_skopal.dao.DaoCompte;
 import licence.pro.td_banque_skopal.factories.FactoryCompte;
+import licence.pro.td_banque_skopal.singletons.SCompteTools;
 
 public class TestCompteTools {
 	
@@ -25,6 +30,29 @@ private Compte instance;
 	@Test
 	public void TestCompteTools_isInstanceCompte() {
 		assertTrue(instance instanceof Compte);
+	}
+	
+	@Test 
+	public void testCompteTools_retraitNegativeTrueShouldSuccess() {
+		
+		CompteTools compteTools = SCompteTools.getInstance();
+		
+		//  create mock
+        DaoCompte daoCompte = mock(DaoCompte.class);
+        
+        // define behavior 
+        when(daoCompte.getCompteById(42)).thenReturn( FactoryCompte.getCompte(42, 3630, 100.0, true) );
+                
+        Compte compte = daoCompte.getCompteById(42);
+        compteTools.retrait(compte, 60);
+
+        // assertEquals(60.0, compte.getBalance());		
+        boolean result = false;
+		if (40.0 == compte.getBalance()) {
+			result = true;
+		}
+		assertTrue(result);
+				
 	}
 	
 }
